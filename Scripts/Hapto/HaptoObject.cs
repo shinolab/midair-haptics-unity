ea9f5239a27b3254705bdcfb4f4ff934fad729ed
matsubayashi <matsubayashi@hapis.k.u-tokyo.ac.jp>
2025-10-16 14:34:59 +0900

    separate softbody package

--- Scripts/Hapto/HaptoObject.cs
+++ Scripts/Hapto/HaptoObject.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Obi;
-using UnityEngine.Assertions.Must;
 
 
 public class HaptoObject : MonoBehaviour
--- Scripts/Hapto/HaptoRigid.cs
+++ Scripts/Hapto/HaptoRigid.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
-using Obi;
 
 public class HaptoRigid : HaptoObject
 {
